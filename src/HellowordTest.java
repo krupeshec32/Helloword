9132ecdc154e179d2c624bcb78d65ed969e4bf30
krupesh <krup_32@yahoo.com>
2018-07-17 22:03:18 -0400

    junit comment

--- src/HellowordTest.java
+++ src/HellowordTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+//this is test case of junit
+
 class HellowordTest {
 
 	@Test
